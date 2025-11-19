b7b33fa4a90ba3ea7339e5eea07da8261b69e6f8
Diego <dpadula@github.com>
2025-11-18 22:18:41 -0300

    Hay errores no sé si en el id o en la api

--- api/movies.ts
+++ api/movies.ts
@@ -1,19 +1,41 @@
+const apiKey =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZDdlMjM4MjkyNTU1YmI2MTJkODQxNjY2ODU2Y2VmOSIsIm5iZiI6MTY1OTAxMzYzOS45NjIsInN1YiI6IjYyZTI4YTA3ZDM3MTk3MDA0YzZiN2ViNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.t53YLqCrdK0u292OytiNeHzntnc-YrHsEY3oKEa8DRc';
+
+const headers = {
+  accept: 'application/json',
+  Authorization: `Bearer ${apiKey}`,
+};
+
 export const fetchTopRatedMovies = async () => {
   const url = 'https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1';
   const options = {
     method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZDdlMjM4MjkyNTU1YmI2MTJkODQxNjY2ODU2Y2VmOSIsIm5iZiI6MTY1OTAxMzYzOS45NjIsInN1YiI6IjYyZTI4YTA3ZDM3MTk3MDA0YzZiN2ViNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.t53YLqCrdK0u292OytiNeHzntnc-YrHsEY3oKEa8DRc',
-    },
+    headers: headers,
   };
 
   try {
     const response = await fetch(url, options);
     const data = await response.json();
-    console.log(JSON.stringify(data, null, 2));
     return data.results;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const fetchMovieDetails = async (id: number) => {
+  console.log('🚀 ~ fetchMovieDetails ~ id:', id);
+  const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+
+  const options = {
+    method: 'GET',
+    headers: headers,
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log('🚀 ~ fetchMovieDetails ~ data:', data);
+    return data;
   } catch (error) {
     console.error(error);
   }
--- app/[id].tsx
+++ app/[id].tsx
@@ -1,12 +1,38 @@
+import { useQuery } from '@tanstack/react-query';
 import { useLocalSearchParams } from 'expo-router/build/hooks';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { fetchMovieDetails } from '../api/movies';
 
 const MovieDetails = () => {
   const { id } = useLocalSearchParams();
+  console.log('🚀 ~ MovieDetails ~ id:', id);
+
+  // normalize id which can be string | string[] | undefined to a single string or undefined
+  const idParam = Array.isArray(id) ? id[0] : id;
+  const movieId = idParam ? Number(idParam) : undefined;
+
+  const {
+    data: movie,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ['movies', idParam],
+    queryFn: () => fetchMovieDetails(movieId as number),
+  });
+  console.log('🚀 ~ MovieDetails ~ movie:', movie);
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (isError) {
+    return <Text>{error.message}</Text>;
+  }
   return (
     <View>
-      <Text>MovieDetails {id}</Text>
+      <Text>MovieDetails {movie.title}</Text>
     </View>
   );
 };
